59767e9036817a2caf69a310233e88c198f4944c
Pnschubel <pnschubel@stu.naperville203.org>
2018-09-25 12:22:38 -0500

    DNAGenerator finished
    Finished DNA generator

--- DNAGenerator.java
+++ DNAGenerator.java
@@ -1,3 +1,5 @@
+import java.util.Collections;
+import java.util.ArrayList;
 /**
  * Will Generate a DNA sequence, split the sequence into pieces, and shuffle the resulting pieces
  * (Setup for the shotgun sequencing)
@@ -10,13 +12,15 @@ public class DNAGenerator
 {
     /**String[] NUCLEOTIDES - the possible nucleotides in DNA sequence
      * String sequence - the sequence of DNA nucleotides 
-     * String[] pieces - pieces of the DNA sequence
      * int THRESHHOLD - the minimum size of a piece of overlap 
+     * int MINSIZE - the minimum size of the sequence
+     * int MAXSIZE - the maximum size of the sequence
     */
     private String[] NUCLEOTIDES = {"A","T","C","G"};
     private String sequence;
-    private String[] pieces;
     int THRESHHOLD = 10;
+    int MINSIZE = 1500;
+    int MAXSIZE = 3000;
 
     /**
      * Default constructor for objects of class DNAGenerator
@@ -25,7 +29,7 @@ public class DNAGenerator
     {
         // initialise instance variables
         sequence = "";
-        pieces = new String[];
+        this.getDNA();
     }
 
     /**
@@ -40,7 +44,7 @@ public class DNAGenerator
          * int size = the overall size of the DNA sequence (between 
          * int pickBase = index of chosen nucleotide (0-3);
          */
-        int size = (int)((Math.random() * 1000) + 1001);
+        int size = (int)((Math.random() * MINSIZE) + (MAXSIZE - MINSIZE));
         int pickBase;
         
         //Fills up the sequence with nucleotides
@@ -57,16 +61,57 @@ public class DNAGenerator
      *      Will be somewhere between 10 and 25 pieces overlap per side.
      * @pre String[] pieces is empty
      * @param none
-     * @return none
+     * @return ArrayList<String> the pieces of the DNA
      */
-    private void breakDNA(){
+    public ArrayList<String> breakDNA(){
         int start = 0; //Location along the sequence where chunk starts.
-        int end = (int)(Math.random() * 39 + 51); // end location of chunk: gives it size from 51-90
+        int end = (int)(Math.random() * 51 + 39); // end location of chunk: gives it size from 51-90
+        ArrayList<String> pieces = new ArrayList(); // what we hold pieces of DNA in
         
-        while (end != sequence.length() -1 ){
-        //FINSIH LATER.
+        //Goes through sequence
+        //Means that the end segment of DNA will have at least THRESHHOLD unique
+        //nucleotides at its end.
+        while (end < sequence.length() - THRESHHOLD){
+            //Adds the new piece of DNA to the pieces.
+            pieces.add(sequence.substring(start, end));
+            
+            //Updates the start and end coordinates
+            //The bit at the end guarentees at least THRESSHOLD bit overlap
+            start = end - (int)(Math.random() * 15 + THRESHHOLD);
+            //Little bit less variable than original end due to overlap
+            //So we made it a bit shorter.
+            end += (int)(Math.random() * 26 + 39);
         }
+        //Gets the remaining nucleotides into the pieces array
+        pieces.add(sequence.substring(start, sequence.length()));
         
+        //shuffles the array
+        Collections.shuffle(pieces);
+        System.out.println(pieces);
+        System.out.println(sequence);
+        System.out.println(pieces.size());
+        System.out.println(sequence.length());
+        
+        //Returns the pieces array
+        return pieces;
+    }
+    
+    /**
+     * Checks to see if the given nucleotide sequence is the same at the true sequence
+     * @param String a sequence of nucleotides to compare to the true sequence
+     * @return boolean whether or not the strings match
+     */
+    public boolean isSequence(String falseSequence){
+        return sequence.equals(falseSequence);
+    }
+    
+    /**
+     * Gives the size of the Threshhold of overlap
+     * @param none
+     * @return int the minimum amount of overlap of the sequence
+     */
+    public int getThreshhold(){
+        return THRESHHOLD;
     }
 }
 /*
---
+++ DNATest.java
@@ -0,0 +1,13 @@
+/**
+ * Tests for the Shotgun Sequencing Lab
+ * 
+ * @Payton Schubel
+ * @1.0 (Start: 9/25/2018)
+ */
+public class DNATest
+{
+    public static void main(String[] args){
+        DNAGenerator myDNA = new DNAGenerator();
+        myDNA.breakDNA();
+    }
+}
