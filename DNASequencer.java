c31da7475861f4cf30c8269fedc24104b5385c0d
Pnschubel <pnschubel@stu.naperville203.org>
2018-10-12 11:36:02 -0500

    Worked on DNA Sequencer
    Wrote the starts method and began the ends and reverse method.

--- DNASequencer.java
+++ DNASequencer.java
@@ -1,3 +1,6 @@
+import java.util.Set;
+import java.util.TreeSet;
+import java.util.LinkedList;
 /**
  * Will sequence an array list of nucleotides with overlapping ends.
  * 
@@ -15,7 +18,9 @@ public class DNASequencer
     /**Final resequenced DNA**/
     private String resequencedDNA;
     
-    int CUTTOFF = 25; //The max amount of overlap we care about -- can alter if not enough
+    /**A place to keep all the indexes of the pieces to be added in order**/
+    private LinkedList indexes;
+    
     
     /**
      * Initializes the DNASequencer()
@@ -28,6 +33,46 @@ public class DNASequencer
         end = new TreeSet();     
         //Gets the DNA pieces
         pieces = parts;
+        
+        //Creates the string for the resequenced DNA
+        resequencedDNA = "";
+        
+        //Creates the LinkedList for the indexes of the DNA in order.
+        indexes = new LinkedList();
+    }
+    
+    /**
+     * Method to reverse a string
+     * @param String the string you want to reverse
+     * @return String the reversed String
+     */
+    private String reverse(String rightways){
+        String backwards = "";//String to put in backwards characters.
+    }
+    
+    /**
+     * Method to create set of the first part of the pieces as well as the index
+     * @param none
+     * @return none
+     */
+    private void starts(){
+        //i is the index of the piece in pieces
+        for (int i = 0; i < pieces.size; i++){
+            //Takes the first three nucleotides and the index, put them in the set.
+            start.add(pieces[i].substring(0,3) + " " + i);
+        }
+    }
+    
+    /**
+     * Method to create the set of the last part of the array in reverse order and index
+     * @param none
+     * @return none
+     */
+    private void ends(){
+        //i is index of the piece in pieces
+        for (int i = 0; i < pieces.size; i++){
+            end.add(pieces[i].charAt(
+        }
     }
 
     /**
